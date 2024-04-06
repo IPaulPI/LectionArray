d56c964294e0b3cf4b75d8ca7db82b437cb30c1e
Pavel Peredrij <peredrij.pavel19@gmail.com>
2024-04-06 12:14:19 +0300

    Add sortingArray

---
+++ lection050424/method/sortingArray/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] arr = { 3, 5, 7, 9, 4, 6, 8, 1, 2 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($" {array[i]} ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
